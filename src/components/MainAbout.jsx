f31291195dc47a74f79cdc15a4788a2e0d45237b
Arghya Dutta <102239340+arghyadutta080@users.noreply.github.com>
2023-01-07 12:47:21 +0530

    Update MainAbout.jsx

--- src/components/MainAbout.jsx
+++ src/components/MainAbout.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import Navbar from './Navbar'
 import about_bg_1 from './Images/about_bg_1.jpeg'
-import about_bg_2 from './Images/about_bg_2.jpeg'
 import Footer from './Footer'
 
 function MainAbout() {
@@ -18,16 +17,16 @@ function MainAbout() {
                         <p className="">The School of Art offers an undergraduate art major for students in Yale College (see the bulletin Yale College Programs of Study). In addition, the School’s courses are open to students in the Graduate School of Arts and Sciences and other professional schools of the University, and School of Art students may enroll in elective courses in the Graduate School and other professional schools as well as in the College with permission.</p>
                     </div>
                 </div>
-                <div className="p-3 mb-4 rounded-3 flex-wrap shadow-lg" style={{ backgroundColor: "#ffffcc" }}>
+                <div className="p-3 mb-4 rounded-3 flex-wrap shadow-lg" style={{ backgroundColor: "#ccffff" }}>
                     <div className="container-fluid py-3">
-                        <h4 className="" style={{ color: "#800000" }}>MASTER OF FINE ARTS DEGREE (MFAS)</h4>
+                        <h4 className="" style={{ color: "#00004d" }}>MASTER OF FINE ARTS DEGREE (MFAS)</h4>
                         <p className="">The degree of Master of Fine Arts is the only degree offered by the School of Art. It is conferred by the University upon recommendation of the faculty after successful completion of all course work in residence and after a thesis presentation that has been approved by the faculty. It implies distinctive achievement on the part of students in studies in the professional area of their choice and demonstrated capacity for independent work. The minimum residence requirement is two years. All candidates’ work is reviewed by faculty at the end of each term. If the work is not considered satisfactory, the student may not be invited back to complete the program (see section on Review and Awards under Academic Regulations in the chapter General Information). All degree candidates are expected to be present at the Commencement exercises in May unless excused by the dean.</p>
                         <p className=''>Course work for the Master of Fine Arts degree carries a minimum of sixty credits. The disposition of these credits varies according to the area of study and is agreed upon at the time of registration between the student and the student’s faculty adviser.</p>
                     </div>
                 </div>
-                <div className="p-3 mb-4 rounded-3 flex-wrap shadow-lg" style={{ backgroundColor: "#ffffcc" }}>
+                <div className="p-3 mb-4 rounded-3 flex-wrap shadow-lg" style={{ backgroundColor: "#ffe6ff" }}>
                     <div className="container-fluid py-3">
-                        <h4 className="" style={{ color: "#800000" }}>PUBLIC LECTURES AND EXHIBITIONS</h4>
+                        <h4 className="" style={{ color: "#660066" }}>PUBLIC LECTURES AND EXHIBITIONS</h4>
                         <p className="">Each department has its own visitors program in which professionals from outside the School are invited to lecture to students or take part in critiques.</p>
                         <p className=''>The School also facilitates a public all-school lecture program, in which ideas of general and cross-disciplinary importance are explored by visiting artists and members of the faculty in events typically hosted in the evening that are free and open to the public.</p>
                         <p className="">The School of Art’s galleries in Green Hall and EIK at 32 Edgewood Avenue provide a year-round forum for the exhibition of work by students, faculty, and special guests in the four graduate departments of the School and the undergraduate program. Green Gallery is open to the public when exhibitions are scheduled, but open hours vary by semester. EIK is open during limited hours for specific exhibitions and events. Please refer to the Gallery & Publications page for more information.</p>
